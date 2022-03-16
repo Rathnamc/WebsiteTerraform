5b9bb30644b540d0a6a936cad0971173728a055f
Chris Rathnam <rathnam.c@gmail.com>
2022-03-15 20:24:42 -0400

    update

--- main.tf
+++ main.tf
@@ -15,32 +15,33 @@ variable "is_temp_env" {
 }
 
 resource "aws_s3_bucket" "b" {
-    bucket = "${var.env_prefix}-website-acd-1"
-    acl    = "public-read"
-    force_destroy = var.is_temp_env
+  bucket = "${var.env_prefix}website-acd-1"
+  acl    = "public-read"
+  force_destroy = var.is_temp_env
 
-    policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": "s3:GetObject",
-        "Resource": "arn:aws:s3:::${var.env_prefix}-website-acd-1/*"
-      }
-    ]
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.env_prefix}website-acd-1/*"
+    }
+  ]
+}
+  POLICY
+
+  website {
+    index_document = "index.html"
   }
-    POLICY
 
-    website {
-        index_document = "index.html"
-    }
-
-    tags = {
-        ManagedBy = "terraform"
-    }
+  tags = {
+    ManagedBy = "terraform"
+  }
+}
 
 output "website" {
   value = "http://${aws_s3_bucket.b.website_endpoint}"
