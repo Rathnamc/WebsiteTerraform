55a7adecf44f735d9612ba532176a458fa1689fb
Chris Rathnam <rathnam.c@gmail.com>
2022-03-15 20:18:04 -0400

    Complete TF

--- main.tf
+++ main.tf
@@ -17,5 +17,27 @@ variable "is_temp_env" {
 resource "aws_s3_bucket" "b" {
     bucket = "${var.env_prefix}-website-acd-1"
     acl    = "public-read"
-  
-}
+    force_destroy = var.is_temp_env
+
+    policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadGetObject",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::${var.env_prefix}-website-acd-1/*"
+      }
+    ]
+  }
+    POLICY
+
+    website {
+        index_document = "index.html"
+    }
+
+    tags = {
+        ManagedBy = "terraform"
+    }
